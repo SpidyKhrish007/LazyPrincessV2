669ddd9e901a842f92236e33d758ff68e2508bed
Khrish <106378292+SpidyKhrish007@users.noreply.github.com>
2023-09-08 20:41:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ API_HASH = environ.get('API_HASH', '19f8365d98fb11c9cd6c1eaa8b1fa4b8')
 BOT_TOKEN = environ.get('BOT_TOKEN', "6384094059:AAFbvaJ2jb_AHfb1xTATuZvciJMnaH9EZws")
 
 #Port
-PORT = environ.get("PORT", "8080")
+PORT = environ.get("PORT", "9000")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
